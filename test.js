4a34796706e8792278f7bf3d55c37e999998f77a
Jason Dobry <jason.dobry@gmail.com>
2014-06-13 21:02:58 -0600

    Removed test file.

--- test.js
+++ test.js
@@ -1,25 +1,7 @@
-var Disqus = require('./lib');
-
-var disqus = new Disqus({
-	https: true,
-	logLevel: 'debug',
-	api_key: 'IfpbvNk1xFLfNnNvdcWg0nrcYpuCIayQcPchc9emFCXaRWFLLQi9AEadNfKMh6Jd',
-	api_secret: 'wMljoyWuIczupvXE3TySZz0rAmTyeN7QQArxVJTKbG63uTXzbMnOO8dCx4cGDF3I'
-});
-
-disqus.trends.listThreads({
-	limit: 1
-})
-	.then(function (res) {
-		console.log(res);
-	})
-	.catch(function (err) {
-		console.log('CATCH');
-		console.error(err);
-		process.exit(1);
-	})
-	.error(function (err) {
-		console.log('ERROR');
-		console.log(err);
-		process.exit(1);
-	});
+/**
+ * Created with IntelliJ IDEA.
+ * User: jason
+ * Date: 6/13/14
+ * Time: 9:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
